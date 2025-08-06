7df40ae46ced9082003e54f252ae462ce418e3aa
Adam Marek <adam.marek@intel.com>
2025-08-07 00:31:51 +0200

    back button added to device details screem

--- src/guis/gui_devices.cpp
+++ src/guis/gui_devices.cpp
@@ -18,32 +18,34 @@ namespace {
     config::KeyMap deviceMap;
 }
 
-static void clear_content()
+static void clear()
 {
     if(content) {
-        lv_obj_del(content);
-        content = nullptr;
+        lv_obj_clean(content);
+    }
+    if(header) {
+        lv_obj_clean(header);
     }
 }
 
-void build_devices_tab(lv_obj_t* parent)          // typedef create_tab_content
+void build_devices_tab(lv_obj_t* parent)
 {
-    /* root & header are created once per tab lifetime ------------------ */
-    root   = parent;
+    omote_log_d("Buidling devices tab");
+    root = parent;
     header = lv_obj_create(root);
     lv_obj_set_size(header, LV_PCT(100), LV_PCT(20));
     lv_obj_set_style_bg_opa(header, LV_OPA_COVER, 0);
     lv_obj_set_style_pad_left(header, 4, 0);
     lv_obj_align(header, LV_ALIGN_TOP_MID, 0, 0);
 
-    show_device_list();                           // initial view
+    show_device_list();
 }
 void show_device_list()
 {
-    // clear_content();
+    clear();
+     
     lv_label_set_text(lv_label_create(header), "Devices");
-    //lv_obj_set_style_text_font(header, &lv_font_montserrat_10, LV_PART_MAIN);
-
+ 
     content = lv_list_create(root);
     lv_obj_set_layout(content, LV_LAYOUT_FLEX);
     lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
@@ -65,6 +67,9 @@ void show_device_list()
 void destroy_devices_tab()
 {
     //lv_obj_clean(root);
+    root = nullptr;
+    header = nullptr;
+    content = nullptr;
 }
 
 void register_gui_devices()
@@ -87,21 +92,31 @@ void cmd_btn_cb(lv_event_t* e)
 
 void show_device_details(config::Device& entry)
 {
-    clear_content();
-    lv_obj_clean(header);
-    // lv_obj_t* back = lv_btn_create(header);
-    // lv_obj_set_size(back, 36, 36);
-    // lv_label_set_text(lv_label_create(back), LV_SYMBOL_LEFT);
-    // lv_obj_add_event_cb(back, [](lv_event_t*){ show_device_list(); },
-    //                     LV_EVENT_CLICKED, nullptr);    
+    clear();
     
-    deviceMap.swap(entry.defaultKeys);
-    /* Title */
-    lv_label_set_text_fmt(lv_label_create(header), "%s", entry.displayName());
-    // lv_obj_t* lbl = lv_label_create(header);
-    // lv_label_set_text_fmt(lbl, "%s", entry.displayName());
-    // lv_obj_align(lbl, LV_ALIGN_TOP_MID, 0, 6);
+    /* Back button */
+    lv_obj_t* back = lv_btn_create(header);
+    lv_obj_set_size(back, 25, 25);
+    lv_obj_align(back, LV_ALIGN_LEFT_MID, 4, 0);
+    lv_obj_add_event_cb(back, [](lv_event_t* e){
+        show_device_list();
+    }, LV_EVENT_CLICKED, nullptr);
+    lv_obj_t* back_label = lv_label_create(back);
+    lv_label_set_text(back_label, LV_SYMBOL_LEFT);
+    lv_obj_center(back_label);
 
+    /* Title */
+    lv_obj_t* title = lv_label_create(header);
+    lv_label_set_text_fmt(title, "%s", entry.displayName());
+    lv_obj_align(title, LV_ALIGN_LEFT_MID, 44, 0);
+
+
+
+    // /* Title */    
+    // lv_label_set_text_fmt(lv_label_create(header), "%s", entry.displayName());
+
+
+    deviceMap.swap(entry.defaultKeys);
     /* List of command buttons */
     content = lv_obj_create(root);
     lv_obj_set_size(content, LV_PCT(100), LV_PCT(85));
@@ -119,7 +134,7 @@ void show_device_details(config::Device& entry)
         lv_obj_add_event_cb(b, cmd_btn_cb, LV_EVENT_CLICKED, (void*)&cmd);
 
         lv_label_set_text(lv_label_create(b), cmd.displayName());
-        omote_log_i("Button created: %s\n", cmd.displayName());
+        omote_log_i("Button created: %s (%d)", cmd.displayName(), cmd.ID);
 
     }
     if(!any) {
