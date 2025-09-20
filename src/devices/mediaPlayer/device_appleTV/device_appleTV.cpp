a56be74992c13a4518c7f7064b7f86897b6bcc1f
Andrew Stucky <andrew.david.stucky@gmail.com>
2025-09-20 14:21:20 -0400

    chore: updates formatting for appleTV device.

--- src/devices/mediaPlayer/device_appleTV/device_appleTV.cpp
+++ src/devices/mediaPlayer/device_appleTV/device_appleTV.cpp
@@ -3,7 +3,7 @@
 #include "applicationInternal/hardware/hardwarePresenter.h"
 #include "device_appleTV.h"
 
-#if (ENABLE_HUB_COMMUNICATION == 1)
+#if (ENABLE_HUB_COMMUNICATION > 0)
 #include "devices/misc/device_hub_helper.h"
 #endif
 
@@ -32,22 +32,7 @@ uint16_t APPLETV_PLAY_PAUSE;
 uint16_t APPLETV_STOP;
 
 void register_device_appleTV() {
-    #if (ENABLE_HUB_COMMUNICATION == 1)
-    // Register hub commands for Apple TV when Hub is enabled
-    register_hub_command(&APPLETV_POWER_ON, "APPLE_TV", "POWER_ON");
-    register_hub_command(&APPLETV_POWER_OFF, "APPLE_TV", "POWER_OFF");
-    register_hub_command(&APPLETV_PLAY_PAUSE, "APPLE_TV", "PLAY_PAUSE");
-    register_hub_command(&APPLETV_SKIP_FORWARD, "APPLE_TV", "SKIP_FORWARD");
-    register_hub_command(&APPLETV_SKIP_BACKWARD, "APPLE_TV", "SKIP_BACKWARD");
-    register_hub_command(&APPLETV_STOP, "APPLE_TV", "STOP");
-    register_hub_command(&APPLETV_UP, "APPLE_TV", "UP"); 
-    register_hub_command(&APPLETV_DOWN, "APPLE_TV", "DOWN");
-    register_hub_command(&APPLETV_RIGHT, "APPLE_TV", "RIGHT");
-    register_hub_command(&APPLETV_LEFT, "APPLE_TV", "LEFT");
-    register_hub_command(&APPLETV_SELECT, "APPLE_TV", "SELECT");
-    register_hub_command(&APPLETV_MENU, "APPLE_TV", "MENU");
-    register_hub_command(&APPLETV_HOME, "APPLE_TV", "HOME");
-  #else
+  #if (ENABLE_HUB_COMMUNICATION == 0)
     register_command(&APPLETV_UP                   , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E15080"}));
     register_command(&APPLETV_DOWN                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E13080"}));
     register_command(&APPLETV_LEFT                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E19080"}));
@@ -65,5 +50,20 @@ void register_device_appleTV() {
     
     register_command(&APPLETV_MENU                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0x77E1C080"}));
     register_command(&APPLETV_HOME                 , makeCommandData(IR, {std::to_string(IR_PROTOCOL_NEC), "0xA7E10280:32:1"})); // Code + kNECBits + 1 repeat
+  #else
+    // Register hub commands for Apple TV when Hub is enabled
+    register_hub_command(&APPLETV_POWER_ON, "APPLE_TV", "POWER_ON");
+    register_hub_command(&APPLETV_POWER_OFF, "APPLE_TV", "POWER_OFF");
+    register_hub_command(&APPLETV_PLAY_PAUSE, "APPLE_TV", "PLAY_PAUSE");
+    register_hub_command(&APPLETV_SKIP_FORWARD, "APPLE_TV", "SKIP_FORWARD");
+    register_hub_command(&APPLETV_SKIP_BACKWARD, "APPLE_TV", "SKIP_BACKWARD");
+    register_hub_command(&APPLETV_STOP, "APPLE_TV", "STOP");
+    register_hub_command(&APPLETV_UP, "APPLE_TV", "UP"); 
+    register_hub_command(&APPLETV_DOWN, "APPLE_TV", "DOWN");
+    register_hub_command(&APPLETV_RIGHT, "APPLE_TV", "RIGHT");
+    register_hub_command(&APPLETV_LEFT, "APPLE_TV", "LEFT");
+    register_hub_command(&APPLETV_SELECT, "APPLE_TV", "SELECT");
+    register_hub_command(&APPLETV_MENU, "APPLE_TV", "MENU");
+    register_hub_command(&APPLETV_HOME, "APPLE_TV", "HOME");
   #endif
 }
